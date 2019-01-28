2a8761ca13af8e228169762dd5e43ac1cb691c75
Ammar Ali <funnyguy118@gmail.com>
2019-01-28 18:18:27 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,10 +2,10 @@ def my_select(collection)
   i = 0 
   selects = []
   while i < collection.length 
-      if yield(collection[i]) == TRUE
-        selects << collection [i]
-      end
-      i += 1
+    if yield(collection[i]) == TRUE
+      selects << collection[i]
+    end
+    i+=1
   end
   selects
 end
